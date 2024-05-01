1b52faafeac2f083460bc58caf5d4f7736e6a755
Hr Habib <mdhabibur.hr7@gmail.com>
2024-05-01 10:44:39 +0600

    wow I have explored basic data types in ts

---
+++ module/src/1.4.ts
@@ -0,0 +1,8 @@
+// basic data type 
+
+let customerName: string = "shakib";
+let myAge: number = 22;
+const isMarried: boolean = false;
+const numbersList: number[] = [12, 12, 32, 12];
+const nameList: string[] = ['sakib', "rakib", "akib"];
+const dataType: [number, string, boolean] = [ 12, "akib", true]
---
+++ module/src/test.ts
@@ -0,0 +1,1 @@
+const customerAge = 33;
