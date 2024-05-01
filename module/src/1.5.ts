a570ca6ca2912b7307abdf5d6375294fbd3e2a2c
Hr Habib <mdhabibur.hr7@gmail.com>
2024-05-01 10:56:00 +0600

    object and object property explored

---
+++ module/src/1.5.ts
@@ -0,0 +1,10 @@
+const user : {
+    company: 'Programming Hero',
+    readonly firstName: string,
+    middleName?: string,
+    lastName: string,
+} = {
+    company:"Programming Hero",
+    firstName:"Habibur",
+    lastName:"rahman"
+}
