659b27d89110802f94016c526149349c081b96df
Hr Habib <mdhabibur.hr7@gmail.com>
2024-05-01 11:11:17 +0600

    array explored in ts

---
+++ module/src/1.6.ts
@@ -0,0 +1,15 @@
+function add(num1:number, num2:number):number ={
+    return num1 + num2;
+};
+
+const add2 = (num1:number, num2:number):number => num1+num2;
+const poorUser =  {
+    name:"habib",
+    balance: 30,
+    addBalance(balance:number):number{
+        return this.balance + balance;
+    }
+}
+
+const arr: number[] = [1,2,3,4];
+const sqArray:number[] = arr.map((elem : number)=> elem*elem);
