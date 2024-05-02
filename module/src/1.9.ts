1ba4021f20e616c8ab7873a986568eb235f51b8e
Hr Habib <mdhabibur.hr7@gmail.com>
2024-05-02 11:36:08 +0600

    new code added

---
+++ module/src/1.9.ts
@@ -0,0 +1,22 @@
+{
+// type alias
+type Student ={
+    name:string,
+    age:number,
+    contactNo?: string,
+    gender:string,
+    address:string,
+}
+
+const student1: Student = {
+    name:"habib",
+    age:21,
+    gender: 'male',
+    address:'sylhet'
+}
+
+type UserName = string;
+const userName : UserName = "ahmed"
+
+    // 
+}
